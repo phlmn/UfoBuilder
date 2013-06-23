8ecac206b73abb0d5cc634552172338b3e7ad41a
bart <published@email.de>
2013-06-23 22:43:41 +0200

    class Diagramm added

--- UFO Builder/UfoEditor.h
+++ UFO Builder/UfoEditor.h
@@ -1,7 +1,13 @@
 #pragma once
 
+#include <list>
+
+class UfoCatalogObject;
+
 class UfoEditor
 {
+private:
+	std::list<UfoCatalogObject*> m_ufoCatalogObjects;
 public:
 	UfoEditor();
 	~UfoEditor();
